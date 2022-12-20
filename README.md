85d8094c81e82673f104b2858d7bf3fa92533ead
Lord Evan <114256148+LordEvanElias@users.noreply.github.com>
2022-12-19 21:48:43 -0600

    Added code to create the various sections of my README with prompts using inquirer.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# README-maker
+# noasdf
+  <img src="https://img.shields.io/badge/license-None-blue.svg">
+
+  # Description
+
+  asdf
+
+  # Table of Contents
+
+  * [Installaton](#installation)
+
+  * [Usage](#usage)
+
+  # Installation
+  asdf
+  undefined
+  ## license 
+  
+  This project is licensed under None
---
+++ index.js
@@ -0,0 +1,61 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    message: "Enter your project title?",
+    name: "title",
+  },
+
+  {
+    type: "input",
+    message: "Describe your project.",
+    name: "description",
+  },
+
+  {
+    type: "input",
+    message: "How do you install your app?",
+    name: "installation",
+  },
+
+  {
+    type: "input",
+    message: "How do you use your project?",
+    name: "usage",
+  },
+
+  {
+    type: "input",
+    message: "Who contributed?",
+    name: "contribution",
+  },
+
+  {
+    type: "input",
+    message: "Enter test instructions.",
+    name: "tests",
+  },
+
+  { type: "list", message: "Select a license for your application.", name: "license", choices: ["None", "APM", "AUR license", "Bower"] },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  const mkdn = generateMarkdown(data);
+  return fs.writeFileSync(fileName, mkdn);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((response) => {
+    console.log(response);
+    writeToFile("README.md", response);
+  });
+}
+
+// Function call to initialize app
+init();
